b9ec979b015c06520e9fec7fb1fe35c95061b15b
Kaleb Wasmuth <kalebwasmuth@gmail.com>
2020-03-25 20:28:19 -0400

    Work on terrain, ship, and audio

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer:MonoBehaviour {
+    [SerializeField] AudioClip themeAudioClip;
+
+    private AudioSource musicSource;
+    private double volumeGoal;
+
+    void Awake() {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        PlayThemeMusic();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessKeyInputs();
+    }
+
+    private void PlayThemeMusic() {
+        musicSource = GetComponent<AudioSource>();
+        musicSource.loop = true;
+        musicSource.volume = 1;
+        musicSource.pitch = 1;
+        volumeGoal = 1;
+        musicSource.PlayOneShot(themeAudioClip);
+    }
+
+    private void ProcessKeyInputs() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            LoadFirstScene();
+        }
+    }
+
+    private void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+}
